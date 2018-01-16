701e100181297abb573c25ea23a8cbd3791df9d9
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-01-16 15:40:30 -0600

    bmi function using ifs.

--- src/1. Introduction/simple.clj
+++ src/1. Introduction/simple.clj
@@ -42,4 +42,27 @@
   (is (= [0 0] (roots 1 0 0)))
   (is (= [-1/4 -1] (roots 4 5 1))))
 
+(defn bmi
+  "Returns the body mass index (as a symbol) given
+  the height a weight."
+  [weight height]
+  (let [BMI (/ weight (* height height))]
+    (if (< BMI 20)
+      'underweight
+      (if (< BMI 25)
+        'normal
+        (if (< BMI 30)
+          'obese1
+          (if (< BMI 40)
+            'obese2
+            'obese3))))))
+
+(deftest test-bmi
+  (is (= 'underweight (bmi 45 1.7)))
+  (is (= 'normal (bmi 55 1.5)))
+  (is (= 'obese1 (bmi 76 1.7)))
+  (is (= 'obese2 (bmi 81 1.6)))
+  (is (= 'obese3 (bmi 120 1.6))))
+
+
 (run-tests)
