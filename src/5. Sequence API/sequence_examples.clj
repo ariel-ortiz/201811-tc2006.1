be349d59cc47bf1017de4fbf3b3ee63b0c8ff9da
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-02-27 17:49:53 -0600

    Alternative implementation of binary function using the thread-last (->>) macro.

--- src/5. Sequence API/sequence_examples.clj
+++ src/5. Sequence API/sequence_examples.clj
@@ -34,10 +34,11 @@
   "Returns a list of binary digits representing the converted
   value of n."
   [n]
-  (second
-    (first
-      (drop-while
-        (fn [[n lst]] (not= n 0))
-        (iterate (fn [[n lst]]
-                   [(quot n 2) (cons (rem n 2) lst)])
-                 [n ()])))))
+  (->>
+    [n ()]
+    (iterate (fn [[n lst]]
+               [(quot n 2) (cons (rem n 2) lst)]))
+    (drop-while
+      (fn [[n lst]] (not= n 0)))
+    first
+    second))
