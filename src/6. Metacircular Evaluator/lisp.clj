0ac3e38b79ff9c4921273b82c6235f8e392cc455
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-03-13 16:11:24 -0600

    Includes function invocations and lambdas.

--- src/6. Metacircular Evaluator/lisp.clj
+++ src/6. Metacircular Evaluator/lisp.clj
@@ -1,3 +1,16 @@
+(import 'clojure.lang.IFn)
+(declare evaluate)
+
+(deftype Closure
+  [env params body]
+  IFn
+  (invoke
+    [self args]
+    (evaluate body
+              (merge env (zipmap params args))))
+  (applyTo
+    [self args]
+    (self args)))
 
 ; Helper functions
 
@@ -38,7 +51,15 @@
       if
       (if (evaluate (second expr) env)
         (evaluate (third expr) env)
-        (evaluate (fourth expr) env)))
+        (evaluate (fourth expr) env))
+
+      lambda
+      (->Closure env (second expr) (third expr))
+
+      ; else, function invocation
+      (apply (evaluate (first expr) env)
+             (map #(evaluate % env)
+                  (rest expr))))
 
     ; 4- Everything else evaluates to itself
     :else
