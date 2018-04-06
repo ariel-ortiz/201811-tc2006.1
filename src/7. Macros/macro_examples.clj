0175a256ddf6e505a2ba9a72ebebe7cb18a6e1a1
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-04-06 15:58:52 -0500

    Redefinition of first macro example using syntax quote.

--- src/7. Macros/macro_examples.clj
+++ src/7. Macros/macro_examples.clj
@@ -2,8 +2,8 @@
   "Debug macro. Evaluates and prints the result
   of an expression."
   [expr]
-  (list 'let ['result expr]
-        (list 'printf "debug %s => %s%n"
-              (list 'quote expr)
-              'result)
-        'result))
+  `(let [result# ~expr]
+     (printf "debug %s => %s%n"
+             (quote ~expr)
+             result#)
+     result#))
