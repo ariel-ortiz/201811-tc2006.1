11b8c68f527e33a05854aca2c7f0633917de90a7
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-04-13 16:07:23 -0500

    More examples using macros.

--- src/7. Macros/macro_examples.clj
+++ src/7. Macros/macro_examples.clj
@@ -7,3 +7,26 @@
              (quote ~expr)
              result#)
      result#))
+
+;;;; Bad code, because both arguments a and b are
+;;;; evaluated before executing the function's body.
+;(defn my-and
+;  [a b]
+;  (if a
+;    b
+;    false))
+
+(defmacro my-and
+  "Implements our own version of the and macro."
+  ([] true)
+  ([a] a)
+  ([a & b]
+   `(let [temp# ~a]
+      (if temp#
+        (my-and ~@b)
+        temp#))))
+
+(defmacro my-comment
+  "Ignores body and returns nil."
+  [& body]
+  nil)
