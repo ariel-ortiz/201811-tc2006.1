b54c22b11ed10949f76e0b64a502662a4c10c873
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-04-17 16:03:17 -0500

    Implementation of reverseo and twin-dupleo logical functions.

--- src/8. miniKanren/minikanren_examples.clj
+++ src/8. miniKanren/minikanren_examples.clj
@@ -11,7 +11,7 @@
         (lasto tail result))]))
 
 (defn dupo
-  "Logical function that succeeds if every element in list
+  "Logical function that succeeds if every element in lst
   is duplicated in result."
   [lst result]
   (conde
@@ -21,3 +21,24 @@
        (conso head tail lst)
        (dupo tail temp)
        (appendo [head head] temp result))]))
+
+(defn reverseo
+  "Logical function that succeeds if the reverse of lst
+  is result."
+  [lst result]
+  (conde
+    [(=== lst [])
+     (=== result [])]
+    [(fresh [head tail temp]
+        (reverseo tail temp)
+        (conso head tail lst)
+        (appendo temp [head] result))]))
+
+(defn twin-dupleo
+  "Logical function that succeeds if lst is a two element
+  sequence where both elements are the same."
+  [lst]
+  (conde
+    [(fresh [h1 t1]
+       (conso h1 t1 lst)
+       (conso h1 [] t1))]))
