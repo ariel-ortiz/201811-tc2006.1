5777db8910224b9a8b7fe2971ade4c2c3c0e16eb
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-04-20 15:16:43 -0500

    Modified logic functions to use pattern matching.

--- src/8. miniKanren/minikanren_examples.clj
+++ src/8. miniKanren/minikanren_examples.clj
@@ -1,44 +1,33 @@
 (use '[clojure.core.logic :rename {== ===}])
 
-(defn lasto
+(defne lasto
   "Logical function that succeed if the last element
   of lst is result."
   [lst result]
-  (conde
-    [(=== lst [result])]
-    [(fresh [head tail]
-        (conso head tail lst)
-        (lasto tail result))]))
+  ([[result] result])
+  ([[head . tail] result]
+   (lasto tail result)))
 
-(defn dupo
+(defne dupo
   "Logical function that succeeds if every element in lst
   is duplicated in result."
   [lst result]
-  (conde
-    [(=== lst [])
-     (=== result [])]
-    [(fresh [head tail temp]
-       (conso head tail lst)
-       (dupo tail temp)
-       (appendo [head head] temp result))]))
+  ([[] []])
+  ([[head . tail] [head head . temp]]
+   (dupo tail temp)))
 
-(defn reverseo
+(defne reverseo
   "Logical function that succeeds if the reverse of lst
   is result."
   [lst result]
-  (conde
-    [(=== lst [])
-     (=== result [])]
-    [(fresh [head tail temp]
-        (reverseo tail temp)
-        (conso head tail lst)
-        (appendo temp [head] result))]))
+  ([[] []])
+  ([[head . tail] result]
+   (fresh [temp]
+     (reverseo tail temp)
+     (appendo temp [head] result))))
 
-(defn twin-dupleo
+(defne twin-dupleo
   "Logical function that succeeds if lst is a two element
   sequence where both elements are the same."
   [lst]
-  (conde
-    [(fresh [h1 t1]
-       (conso h1 t1 lst)
-       (conso h1 [] t1))]))
+  ([[x x]]))
